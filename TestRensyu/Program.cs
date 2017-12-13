c8f03d264ad8dfb61b6a2b1e12d161d011ad1c54
YuTanaka <tanaka@am1.jp>
2017-12-13 13:30:27 +0900

    PaizaなどのC#問題の雛形
    source?.txtにテスト用の入力、test?.txtに期待する出力を作成しておくと、テストで試せる。?部分を通し番号にしておくと全て自動的に読み出してテスト実行する

---
+++ TestRensyu/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRensyu
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine().Trim());
+
+            for (int i = 1; i <= num; i++)
+            {
+                 if (i % 15 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine("" + i);
+                }
+            }
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestRensyu;
+using System.IO;
+using System.Text;
+
+// http://gozuk16.hatenablog.com/entry/2016/05/19/194720
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string srcPath = @"..\..\source";
+        string testPath = @"..\..\test";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var builder = new StringBuilder();
+
+            for (int count = 0; ; count++ )
+            {
+                string fname = srcPath + count + ".txt";
+                if (!File.Exists(fname))
+                {
+                    break;
+                }
+
+                string src = File.ReadAllText(fname);
+                //// コンソールを設定
+                var reader = new StringReader(src);
+                Console.SetIn(reader);
+
+                // 出力の受け取り設定
+                builder.Clear();
+                var writer = new StringWriter(builder);
+                Console.SetOut(writer);
+
+                // テスト実行
+                Program.Main(new string[] { "" });
+
+                // テスト結果読み取り
+                fname = testPath+count+".txt";
+                string expected = File.ReadAllText(fname).Trim();
+
+                // コンソールを読み出す
+                var response = new StringReader(builder.ToString());
+                string actual = response.ReadToEnd().Trim();
+
+                // チェック
+                Assert.AreEqual(expected, actual, "testcase "+count);
+            }
+
+
+
+        }
+    }
+}
